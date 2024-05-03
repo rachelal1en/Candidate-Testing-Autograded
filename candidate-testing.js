be8ef1b8e801562088be09232af22b910ebf1529
Rachel Allen <rqllen@gmail.com>
2024-05-03 17:44:02 -0500

    pushing agains w/workflows enabled

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,7 +7,7 @@ let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
+let candidateAnswer = ""; //
 
 
 //TODO: Variables for Part 2
